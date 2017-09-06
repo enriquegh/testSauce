3fe3f200ac185148baade33e3b44989dc5e3bb65
Enrique Gonzalez <egonzalezh94@gmail.com>
2017-09-06 16:00:23 -0700

    Upload app before test (#5)
    * Added function on Base to upload files to Sauce Storage
    
    This can help make sure application tests have the application uploaded and can ease testing with pre-run executables.
    
    * Grab app name from app capability and upload file to Sauce Storage before class
    
    Had to move some variable permissions because the BeforeClass method needs to be a public static method.
    This makes it better since it will only upload file once for the whole class instead of doing it every method
    
    * Download and use specific apk when testing on Travis
    
    Since Travis can't access the application on my computer, check if it's Travis running the test and download app.
    
    * Fixing Travis working directory where app is stored

--- src/test/java/com/saucelabs/enrique/Test/Base.java
+++ src/test/java/com/saucelabs/enrique/Test/Base.java
@@ -4,6 +4,18 @@ import com.saucelabs.common.SauceOnDemandAuthentication;
 import com.saucelabs.common.SauceOnDemandSessionIdProvider;
 import com.saucelabs.junit.ConcurrentParameterized;
 import com.saucelabs.junit.SauceOnDemandTestWatcher;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.FileEntity;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Ignore;
@@ -12,6 +24,9 @@ import org.junit.rules.TestRule;
 import org.junit.rules.TestWatcher;
 import org.junit.runner.Description;
 import org.junit.runner.RunWith;
+
+import java.io.File;
+import java.io.IOException;
 
 @Ignore
 @RunWith(ConcurrentParameterized.class)
@@ -67,4 +82,42 @@ public abstract class Base implements SauceOnDemandSessionIdProvider {
     public String getSessionId() {
         return sessionId;
     }
+
+    /***
+     * Function that can upload an app or pre-run executable to Sauce Storage
+     * @param sauceFileName Name that file will take on Sauce Storage and be accessed as "sauce-storage:sauceFilename"
+     * @param filePath Complete filepath where file is located
+     */
+    public static void uploadFileSauceStorage(String sauceFileName, File filePath) {
+
+        //:TODO Check if application is there and make overwrite optional
+
+        final String BASE_URL = "https://saucelabs.com/rest/v1/storage/%s/%s?overwrite=true";
+
+        String url = String.format(BASE_URL,USERNAME,sauceFileName);
+
+        CredentialsProvider credProv = new BasicCredentialsProvider();
+
+        credProv.setCredentials(new AuthScope("saucelabs.com",443),
+                                new UsernamePasswordCredentials(USERNAME,ACCESS_KEY));
+
+        FileEntity fileEntity = new FileEntity(filePath);
+
+        HttpPost postRequest = new HttpPost(url);
+        postRequest.setHeader("Content-Type","application/octet-stream");
+        postRequest.setEntity(fileEntity);
+
+
+        try (CloseableHttpClient httpClient = HttpClients.custom().setDefaultCredentialsProvider(credProv).build();
+             CloseableHttpResponse response = httpClient.execute(postRequest)) {
+
+            System.out.println("----------------------------------------");
+            System.out.println(response.getStatusLine());
+            System.out.println(EntityUtils.toString(response.getEntity()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
--- src/test/java/com/saucelabs/enrique/Test/TestAndroid.java
+++ src/test/java/com/saucelabs/enrique/Test/TestAndroid.java
@@ -1,17 +1,39 @@
 package com.saucelabs.enrique.Test;
 
 import io.appium.java_client.android.AndroidElement;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+
+import java.io.File;
 
 public class TestAndroid extends AndroidBase {
 
     public TestAndroid(String deviceName, String platformName, String platformVersion, String browser, String deviceOrientation, String appiumVersion, String app) {
         super(deviceName, platformName, platformVersion, browser, deviceOrientation, appiumVersion, app);
     }
+    @BeforeClass
+    public static void setupApp() {
+        File appFilePath;
+        boolean isContinousIntegration = Boolean.parseBoolean(System.getenv("TRAVIS"));
+
+        if (isContinousIntegration) {
+            String homeDir = System.getenv("TRAVIS_BUILD_DIR");
+            appFilePath = new File(homeDir + "/enrique.apk");
+
+        }
+        else {
+            appFilePath = new File("/Users/enriquegonzalez/saucelabs/apps/Android/enrique.apk");
+
+        }
+
+        uploadFileSauceStorage(appName,appFilePath);
+    }
+
     @Test
     public void sendKeystoApp() {
+
         WebElement username = driver.findElement(By.id("username"));
         WebElement password = driver.findElement(By.id("password"));
 
